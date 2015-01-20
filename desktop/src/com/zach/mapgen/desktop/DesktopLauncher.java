45a69cb444a9eaea9d2880b86f77ba49638e76e8
Zylox <zch5of6@earthlink.net>
2015-01-20 11:48:37 -0600

    first commit

---
+++ desktop/src/com/zach/mapgen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zach.mapgen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zach.mapgen.MapGenerationCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MapGenerationCore(), config);
+	}
+}
